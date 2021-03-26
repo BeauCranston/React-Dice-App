047d638f309a456565cdbd76b5baf118a94da6df
Beau Cranston <beau2tie@gmail.com>
2021-03-26 01:39:47 -0400

    ready to go

--- react-dice-app/src/App.js
+++ react-dice-app/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './Styles/App.css';
-
+import './Styles/main.scss'
 function App() {
   return (
     <div className="App">
-        
+        <h1 className='title'></h1>
     </div>
   );
 }
+
 
 export default App;
